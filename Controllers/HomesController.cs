08c54ac00b33727ca5a0dd3ae7c0d10d4ee92bf1
vamika-rajput <vamikarajput236@gmail.com>
2025-01-20 14:22:35 +0530

    Add project files.

---
+++ Controllers/HomesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrimeMart.Controllers
+{
+    public class HomesController : Controller
+    {
+        // GET: Homes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PrimeMart.MvcApplication" Language="C#" %>
